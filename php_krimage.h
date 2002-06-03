f0ba0bd915c7870eb174f16e7ea62212b5acddb5
JoungKyun Kim <joungkyun@daum.net>
2002-06-03 13:36:53 +0000

    added imgresize_lib function

---
+++ php_krimage.h
@@ -0,0 +1,50 @@
+/*
++----------------------------------------------------------------------+
+| PHP Version 4                                                        |
++----------------------------------------------------------------------+
+| Copyright (c) 1997-2002 The PHP Group                                |
++----------------------------------------------------------------------+
+| This source file is subject to version 2.02 of the PHP license,      |
+| that is bundled with this package in the file LICENSE, and is        |
+| available at through the world-wide-web at                           |
+| http://www.php.net/license/2_02.txt.                                 |
+| If you did not receive a copy of the PHP license and are unable to   |
+| obtain it through the world-wide-web, please send a note to          |
+| license@php.net so we can mail you a copy immediately.               |
++----------------------------------------------------------------------+
+| Author: JoungKyun Kim <http://www.oops.org>                          |
++----------------------------------------------------------------------+
+$Id: php_krimage.h,v 1.1 2002-06-03 13:36:53 oops Exp $
+*/
+#ifndef PHP_KRIMAGE_H
+#define PHP_KRIMAGE_H
+
+#if HAVE_LIBGD
+PHP_FUNCTION(imgresize_lib);
+
+#define	GIFNEWTYPE	1
+#define	PNGNEWTYPE	2
+#define	JPGNEWTYPE	3
+
+/* file type markers */
+PHPAPI const char php_sig_gif[3] = {'G', 'I', 'F'};
+PHPAPI const char php_sig_psd[4] = {'8', 'B', 'P', 'S'};
+PHPAPI const char php_sig_bmp[2] = {'B', 'M'};
+PHPAPI const char php_sig_swf[3] = {'F', 'W', 'S'};
+PHPAPI const char php_sig_jpg[3] = {(char) 0xff, (char) 0xd8, (char) 0xff};
+PHPAPI const char php_sig_png[8] = {(char) 0x89, (char) 0x50, (char) 0x4e, (char) 0x47,
+	(char) 0x0d, (char) 0x0a, (char) 0x1a, (char) 0x0a};
+PHPAPI const char php_sig_tif_ii[4] = {'I','I', (char)0x2A, (char)0x00};
+PHPAPI const char php_sig_tif_mm[4] = {'M','M', (char)0x00, (char)0x2A};
+#endif /* HAVE_LIBGD */
+
+#endif /* PHP_KRFILE_H */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
