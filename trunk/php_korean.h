a572940e2043142dc8f2c98b1aeed17bffe4ec07
oops <oops@e0cc0c3c-65cc-11e0-90e5-803351ca5684>
2002-09-18 10:31:41 +0000

    updated build number

--- trunk/php_korean.h
+++ trunk/php_korean.h
@@ -15,7 +15,7 @@
   | Author:                                                              |
   +----------------------------------------------------------------------+
 
-  $Id: php_korean.h,v 1.19 2002-09-08 13:33:06 oops Exp $
+  $Id: php_korean.h,v 1.20 2002-09-18 10:31:41 oops Exp $
 */
 
 #ifndef PHP_KOREAN_H
@@ -71,7 +71,7 @@ ZEND_END_MODULE_GLOBALS(korean)
 #define KOREAN_G(v) (korean_globals.v)
 #endif
 
-#define BUILDNO "200209082232"
+#define BUILDNO "200209181928"
 #define BUILDVER "0.0.1"
 
 #endif	/* PHP_KOREAN_H */
