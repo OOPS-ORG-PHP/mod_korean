02c55000c8627710ecfc0717ead6936a41c91522
JoungKyun Kim <joungkyun@daum.net>
2003-01-06 13:54:27 +0000

    added script version of korean extension

---
+++ phplib/function_header.php
@@ -0,0 +1,12 @@
+<?
+set_time_limit(0);
+
+if (!extension_loaded(korean)) {
+  include "function_check.php";
+  include "function_filesystem.php";
+  include "function_html.php";
+  include "function_image.php";
+  include "function_mail.php";
+  include "function_parse.php";
+}
+?>
