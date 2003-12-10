a967276168d6c0733d55dc28b08eceea203f8320
JoungKyun Kim <joungkyun@daum.net>
2003-12-10 10:52:34 +0000

    update builddate

--- php_korean.h
+++ php_korean.h
@@ -15,7 +15,7 @@
   | Author:                                                              |
   +----------------------------------------------------------------------+
 
-  $Id: php_korean.h,v 1.59 2003-12-10 10:49:46 oops Exp $
+  $Id: php_korean.h,v 1.60 2003-12-10 10:52:34 oops Exp $
 */
 
 #ifndef PHP_KOREAN_H
@@ -71,7 +71,7 @@ ZEND_END_MODULE_GLOBALS(korean)
 #define KOREAN_G(v) (korean_globals.v)
 #endif
 
-#define BUILDNO "200312022112"
+#define BUILDNO "200312101952"
 #define BUILDVER "0.0.7"
 
 #define phpext_korean_ptr korean_module_ptr
