642ff683dfe2809f274fdade680a4c53f366e14e
JoungKyun Kim <joungkyun@daum.net>
2004-09-14 06:52:44 +0000

    add package init file

---
+++ init.sh
@@ -0,0 +1,3 @@
+rm -rf autom4te.cache/ build/ include/ modules/
+rm -f Makefile.global ac*.m4 config.guess config.h* config.nice config.sub configure*
+rm -f install-sh ltmain.sh missing mkinstalldirs scan_makefile_in.awk
