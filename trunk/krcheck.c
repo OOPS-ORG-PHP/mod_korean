16ede0776dabd0274c93bd80daebe3d126a84122
oops <oops@e0cc0c3c-65cc-11e0-90e5-803351ca5684>
2012-11-06 16:17:18 +0000

    fixed indent

--- trunk/krcheck.c
+++ trunk/krcheck.c
@@ -1,22 +1,19 @@
-/*
-  +----------------------------------------------------------------------+
-  | PHP Version 4                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2002 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 2.02 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available at through the world-wide-web at                           |
-  | http://www.php.net/license/2_02.txt.                                 |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author: JoungKyun Kim <http://www.oops.org>                          |
-  +----------------------------------------------------------------------+
- 
-  $Id$
-*/
+/**
+ * Project: mod_korean ::
+ * File:    krcheck.c
+ *
+ * Copyright (c) 2012 JoungKyun.Kim
+ *
+ * LICENSE: GPL
+ *
+ * @category    Text
+ * @package     mod_korean
+ * @author      JoungKyun.Kim <http://oops.org>
+ * @copyright   2012 OOPS.org
+ * @license     GPL
+ * @version     SVN: $Id$
+ * @since       File available since release 0.0.1
+ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -29,9 +26,10 @@
 #include "php_krcheck.h"
 #include "krregex.h"
 
+/* {{{ Static functions
+ */
 /* {{{ int numberOfchar(char *str) */
-int numberOfchar (char *str, char chk)
-{
+static int numberOfchar (char *str, char chk) {
 	int no, i, len;
 
 	no = 0;
@@ -44,63 +42,54 @@ int numberOfchar (char *str, char chk)
 }
 /* }}} */
 
-/* {{{ unsigned int checkAddr(unsigned char *addr, int type) */
-unsigned int checkAddr(unsigned char *addr, int type)
-{
+/* {{{ unsigned int checkAddr (unsigned char * addr, int type) */
+static unsigned int checkAddr (unsigned char * addr, int type) {
 	regex_t preg;
 	int regRet;
 	unsigned char regex[95];
 	unsigned char regex_e[] = "^[[:alnum:]\xA1-\xFE._-]+@[[:alnum:]\xA1-\xFE_-]+\\.[[:alnum:]._-]+$";
 	unsigned char regex_u[] = "^(http|https|ftp|telnet|news)://[[:alnum:]\xA1-\xFE_-]+\\.[[:alnum:]\xA1-\xFE:&#@=_~%?/.+-]+$";
 
-	if (type == 1) { strcpy(regex, regex_u); }
-    else { strcpy (regex, regex_e); }	
+	strcpy (regex, type ? regex_u : regex_e);
 
-	if (regcomp(&preg, regex, REG_EXTENDED|REG_ICASE) != 0)
-	{
-		php_error(E_WARNING, "Problem REGEX compile in PHP");
+	if ( regcomp (&preg, regex, REG_EXTENDED|REG_ICASE) != 0 ) {
+		php_error (E_WARNING, "Problem REGEX compile in PHP");
 		return 0;
 	}
 
-	regRet = regexec(&preg,addr, 0, NULL, 0);
-	regfree(&preg);
+	regRet = regexec (&preg, addr, 0, NULL, 0);
+	regfree (&preg);
 
-	if(regRet == 0) { return 1; }
-	else { return 0; }
+	return regRet ? 0 : 1;
 }
 /* }}} */
 
-/* {{{ unsigned int chkMetaChar (unsigned char *str, int type) */
-unsigned int chkMetaChar (unsigned char *str, int type)
-{
+/* {{{ unsigned int chkMetaChar (unsigned char * str, int type) */
+static unsigned int chkMetaChar (unsigned char * str, int type) {
 	regex_t preg;
 	int regRet;
 	unsigned char regex[34];
 	unsigned char regex_ur[] = "[^[:alnum:]\xA1-\xFE_-]";
 	unsigned char regex_up[] = "[^[:alnum:]\xA1-\xFE \\._%-]|\\.\\.";
 
-	if (type == 1) { strcpy (regex, regex_up); }
-	else { strcpy (regex, regex_ur); }
+	strpy (regex, type ? regex_up : regex_ur);
 
-	if (regcomp(&preg,regex,REG_EXTENDED|REG_ICASE) != 0)
-   	{
-		php_error(E_WARNING, "Problem REGEX compile in PHP");
+	if ( regcomp (&preg,regex, REG_EXTENDED|REG_ICASE) != 0 ) {
+		php_error (E_WARNING, "Problem REGEX compile in PHP");
 		return 0;
 	}
 
-	regRet = regexec(&preg,str, 0, NULL, 0);
-	regfree(&preg);
+	regRet = regexec (&preg, str, 0, NULL, 0);
+	regfree (&preg);
 
-	if(regRet == 0) { return 1; }
-	else { return 0; }
+	return regRet ? 0 : 1;
 }
 /* }}} */
 
 /* {{{ unsigned int check_table (unsigned char *str) */
-unsigned int check_table (unsigned char *str)
-{
-	unsigned char *buf;
-	unsigned char *regex[] =
+static unsigned int check_table (unsigned char *str) {
+	unsigned char * buf;
+	unsigned char * regex[] =
 	{
 		";[\\d]+;",
 		";<(/?)(TABLE|TH|TR|TD)[^>]*>;i",
@@ -114,59 +103,63 @@ unsigned int check_table (unsigned char *str)
 		";</TABLE>;i",
 		";[\\D]+;"
 	};
-	unsigned char *replace[] = { "", "<\\1\\2>", "1", "2", "3", "4", "94", "93", "92", "91", "" };
+	unsigned char * replace[] = { "", "<\\1\\2>", "1", "2", "3", "4", "94", "93", "92", "91", "" };
 
-	if ( !checkReg(str, "</?[tT][aA][bB][lL][eE][^>]*>") ) return 0;
+	if ( ! checkReg (str, "</?[tT][aA][bB][lL][eE][^>]*>") )
+		return 0;
 
-	buf = (unsigned char *) kr_regex_replace_arr (regex, replace, str, (sizeof (regex) / sizeof (regex[0])));
+	buf = (unsigned char *) kr_regex_replace_arr (
+		regex, replace, str, (sizeof (regex) / sizeof (regex[0]))
+	);
 
-	if ( strlen(buf) % 3 != 0 ) { return 1; }
-	if ( !checkReg(buf, "^12(3|4).+9291$") ) { return 2; }
+	if ( (strlen(buf) % 3) != 0 )
+		return 1;
+	if ( ! checkReg (buf, "^12(3|4).+9291$") ) 
+		return 2;
 
-	while ( checkReg(buf, "([1-4])9\\1") )
-	{
+	while ( checkReg (buf, "([1-4])9\\1") )
 		buf = kr_regex_replace ("/([\\d])9\\1/", "", buf);
-	}
 
-	if (strlen(buf) > 0) { return 3; }
-
-	return 0;
+	return (strlen (buf) > 0) ? 3 : 0;
 }
 /* }}} */
 
 /* {{{ unsigned int multibyte_check(unsigned char *str_o, unsigned int p) */
-unsigned int multibyte_check(unsigned char *str_o, unsigned int p)
-{
-	unsigned char *start_p;
-	unsigned int i, l, twobyte = 0;
+static unsigned int multibyte_check (unsigned char *str_o, unsigned int p) {
+	unsigned char * start_p;
+	unsigned int    i,
+					l,
+					twobyte = 0;
 
 	/* return 0 if point is 1st byte in string */
-	if ( p == 0 ) { return 0; }
+	if ( p == 0 )
+		return 0;
 
-	if (str_o[p] & 0x80 ||
+	if ( str_o[p] & 0x80 ||
 		/* check of 2byte charactor except KSX 1001 range */
 		(str_o[p-1] >= 0x81 && str_o[p-1] <= 0xa0 && str_o[p] >= 0x41 && str_o[p] <=0xfe) ||
-		(str_o[p-1] >= 0xa1 && str_o[p-1] <= 0xc6 && str_o[p] >= 0x41 && str_o[p] <=0xa0))
+		(str_o[p-1] >= 0xa1 && str_o[p-1] <= 0xc6 && str_o[p] >= 0x41 && str_o[p] <=0xa0) )
    	{
-		start_p = strchr(&str_o[p], ' ');
-
 		/* if don't exist ' ' charactor */
-		if(start_p == NULL) { l = strlen(str_o); }
-		else { l = start_p-str_o; }
+		if ( (start_p = strchr (&str_o[p], ' ')) == NULL )
+			l = strlen (str_o);
+		else
+			l = start_p - str_o;
 
-		for (i=p ; i<l ; i++)
-	   	{
-			if (str_o[i] & 0x80) { twobyte++; }
+		for ( i=p ; i<l ; i++ ) {
+			if ( str_o[i] & 0x80 ) {
+				twobyte++;
 			/* 2th byte of 2 byte charactor is not KSX 1001 range */
-			else if ((str_o[i-1] >= 0x81 && str_o[i-1] <= 0xa0 && str_o[i] >= 0x41 && str_o[i] <=0xfe) ||
-					 (str_o[i-1] >= 0xa1 && str_o[i-1] <= 0xc6 && str_o[i] >= 0x41 && str_o[i] <=0xa0))
-		   	{ twobyte++; }
+			} else if (
+				(str_o[i-1] >= 0x81 && str_o[i-1] <= 0xa0 && str_o[i] >= 0x41 && str_o[i] <= 0xfe) ||
+				(str_o[i-1] >= 0xa1 && str_o[i-1] <= 0xc6 && str_o[i] >= 0x41 && str_o[i] <= 0xa0)
+			) {
+				twobyte++;
+			}
 		}
 
-		if (twobyte % 2 != 0)
-	   	{
+		if ( (twobyte % 2) != 0)
 			return 1;
-		}
 	}
 
 	return 0;
@@ -177,13 +170,13 @@ unsigned int multibyte_check(unsigned char *str_o, unsigned int p)
  * type 1 => check of webserver. if iis, return 1. if not return 0
  * type 0 => check of os. if windows, return 1. if not return 0
  */
-unsigned int check_windows(unsigned int type)
-{
-	switch(type)
-   	{
+static unsigned int check_windows (unsigned int type) {
+	switch (type) {
 		case 1:
-			if (sapi_module.name && !strcasecmp(sapi_module.name, "isapi")) { return 1; }
-			else { return 0; }
+			if ( sapi_module.name && ! strcasecmp (sapi_module.name, "isapi") )
+				return 1;
+			else
+				return 0;
 			break;
 		default:
 #ifdef PHP_WIN32
@@ -195,12 +188,13 @@ unsigned int check_windows(unsigned int type)
 	}
 }
 /* }}} */
+/* }}} */
 
 /* {{{ proto string check_uristr_lib(string str)
  *    check uri value that include meta charactors. if include, return 1 nor return 0 */
 PHP_FUNCTION(check_uristr_lib)
 {
-	pval **arg1;
+	zval **arg1;
 	int ret;
 	unsigned char *str;
 
@@ -229,7 +223,7 @@ PHP_FUNCTION(check_uristr_lib)
  *    check filename that include meta charactors. if include, return 1 nor return 0 */
 PHP_FUNCTION(check_filename_lib)
 {
-	pval **arg1;
+	zval **arg1;
 	int ret;
 	unsigned char *filename;
 
@@ -259,7 +253,7 @@ PHP_FUNCTION(check_filename_lib)
  *    check mail address. if mail address is regular, return mail address or return null */
 PHP_FUNCTION(is_email_lib)
 {
-	pval **arg1;
+	zval **arg1;
 	unsigned char *email;
 
 	switch(ZEND_NUM_ARGS())
@@ -290,7 +284,7 @@ PHP_FUNCTION(is_email_lib)
  *    check url. if url is regular, return url address or return null */
 PHP_FUNCTION(is_url_lib)
 {
-	pval **arg1;
+	zval **arg1;
 	unsigned char *url;
 
 	switch(ZEND_NUM_ARGS())
@@ -321,7 +315,7 @@ PHP_FUNCTION(is_url_lib)
  *    check first 1byte is hangul or not. if it is hangul, return 1 nor return 0 */
 PHP_FUNCTION(is_hangul_lib)
 {
-	pval **arg1;
+	zval **arg1;
 	unsigned char *str;
 
 	switch(ZEND_NUM_ARGS())
@@ -352,7 +346,7 @@ PHP_FUNCTION(is_hangul_lib)
  *   check of table structure. if uncomplete structure, return 1 not return 0 */
 PHP_FUNCTION(check_htmltable_lib)
 {
-	pval **arg1;
+	zval **arg1;
 	unsigned int ret;
 
 	switch(ZEND_NUM_ARGS())
