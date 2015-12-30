62377b0c32898047f965b918354e61d776e82c90
JoungKyun Kim <joungkyun@daum.net>
2015-12-30 02:17:19 +0000

    changes copy

--- krerror.c
+++ krerror.c
@@ -12,7 +12,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author: JoungKyun Kim <http://www.oops.org>                          |
+  | Author: JoungKyun Kim <http://oops.org>                          |
   +----------------------------------------------------------------------+
 
   $Id$ 
