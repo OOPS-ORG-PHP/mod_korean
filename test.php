8f2bf15c00ed30726fd1a25ab4dfa616262a85bf
JoungKyun Kim <joungkyun@daum.net>
2016-02-15 07:04:38 +0000

    fixed version

--- test.php
+++ test.php
@@ -20,10 +20,10 @@ $ver = (object) array (
 	'no'   => $m[2][1]
 );
 
-$chk = ($ver->date == '201512280344') ? 'OK' : 'Failed';
+$chk = ($ver->date == '201512301506') ? 'OK' : 'Failed';
 printf (" ** %-40s .. %s\n", 'test buildno_lib()', $chk);
 
-$chk = ($ver->no == '1.0.0') ? 'OK' : 'Failed';
+$chk = ($ver->no == '1.0.1') ? 'OK' : 'Failed';
 printf (" ** %-40s .. %s\n", 'test version_lib()', $chk);
 
 printf (" ** %-40s .. ", 'test movepage_lib() 3sec');
