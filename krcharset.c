ce41a84b6d161cffcbe6bea6fdfdfb67f59e9aa0
JoungKyun Kim <joungkyun@gmail.com>
2019-01-18 02:24:29 +0900

    fixed #10 is_utf8 control reaches end of non-void function on PHP 5

--- krcharset.c
+++ krcharset.c
@@ -630,6 +630,7 @@ int hex2dec (const char * str_o, int type) {
 int is_utf8 (char * s)
 {
 	int len, i, check = 0, j;
+	int ascii_status = 0;
 
 	// utf-8 bomb character
 	if ( s[0] == 0xef && s[1] == 0xbb && s[2] == 0xbf )
@@ -643,6 +644,8 @@ int is_utf8 (char * s)
 		// ASCII 영역은 건너띈다.
 		if ( ! (s[i] & 0x80) )
 			continue;
+
+		ascii_status = 1;
 
 		// utf8 is must 110xxxxxxx
 		// 0x40 -> 01000000
@@ -666,8 +669,13 @@ int is_utf8 (char * s)
 			if ( ((unsigned char) s[j] >> 6) != 0x02 )
 				return 1;
 		}
-		return 0;
+		break;
 	}
+
+	if ( ascii_status == 0 )
+		return 1;
+
+	return 0;
 }
 /* }}} */
 
