9da96e83a7db63d9b28c72129f1cbe1231cf679f
JoungKyun Kim <joungkyun@gmail.com>
2022-02-17 01:52:27 +0900

    enhanced test build environment

--- manage.sh
+++ manage.sh
@@ -68,21 +68,57 @@ case "${mode}" in
 
 		;;
 	test)
+		PHPBIN=/opt/php-qa/php${2}/bin/php
+		PHPIZE=/opt/php-qa/php${2}/bin/phpize
+		PHPCONFIG=/opt/php-qa/php${2}/bin/php-config
+		PHP_OPT="-n"
+
+		if [[ $# == 2 ]]; then
+			./manage.sh clean
+			echo "${PHPIZE} && ./configure"
+			${PHPIZE} && ./configure && make -j8 || exit 0
+		fi
+
+		if (( $2 > 71 )); then
+			PHP_OPT+=" -d 'extension_dir=./modules/' -d 'extension=magic.so'"
+		else
+			PHP_OPT+=" -d 'track_errors=1' -d 'extension_dir=./modules/' -d 'extension=magic.so'"
+		fi
+
 		if [[ -f tests/${3}.php ]]; then
-			/usr/bin/php${2} -d "extension_dir=./modules/" -d "extension=korean.so" tests/${3}.php
+			cat <<-EOL
+				${bgreen}------------------------------------------------------------------------
+				Sample code execution:
+
+				${bcyan}${PHPBIN} ${PHP_OPT} test/${3}.php
+				${bgreen}------------------------------------------------------------------------${normal}
+
+			EOL
+			${PHPBIN} ${PHP_OPT} test/${3}.php
 			exit $?
 		elif [[ -f ${3} ]]; then
-			/usr/bin/php${2} -d "extension_dir=./modules/" -d "extension=korean.so" ${3}
+			cat <<-EOL
+				${bgreen}------------------------------------------------------------------------
+				Sample code execution:
+
+				${bcyan}${PHPBIN} ${PHP_OPT} ${3}
+				${bgreen}------------------------------------------------------------------------${normal}
+
+			EOL
+			eval "${PHPBIN} ${PHP_OPT} ${3}"
 			exit $?
 		fi
 
-		if [[ -z $3 ]]; then
-			./manage.sh clean
-			echo "phpize${2} ./configure"
-			phpize${2} && ./configure --with-libdir=lib64 && make -j8 || exit 0
-		fi
-		echo "make test PHP_EXECUTABLE=/usr/bin/php${2}"
-		make test PHP_EXECUTABLE=/usr/bin/php${2} <<< n
+        cat <<-EOL
+			${bwhite}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			${tcolor}** MAKE test::                                                       ${normal}
+			${bwhite}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+			${bcyan}make test PHP_EXECUTABLE=${PHPBIN}
+			${bwhite}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${normal}
+
+		EOL
+		make test PHP_EXECUTABLE=${PHPBIN} <<< n
 		;;
 	stub)
 		# stub tagging
